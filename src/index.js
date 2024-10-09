02aabaf3bf5568b2b24feafedd87224715ed6600
Orival de Siqueira Junior <orivaljr.2014@gmail.com>
2024-10-09 15:01:02 -0300

    delete import sign in

--- src/index.js
+++ src/index.js
@@ -7,14 +7,6 @@ import { DataStore } from 'aws-amplify/datastore';
 
 import { signIn } from 'aws-amplify/auth';
 
-async function signIn({ username, password }) {
-  try {
-    const { isSignedIn, nextStep } = await signIn({ username, password });
-  } catch (error) {
-    console.log('error signing in', error);
-  }
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
